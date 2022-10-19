e95413e67b22cce66b9ac09929ad5fa4fd3bf119
Joao Lopes <jandrevl@gmail.com>
2022-10-19 21:15:40 +0100

    Finished Measurement Activity layout. Dropped PressureMeasurement class. Starting MeasurementActivity class

--- app/src/main/java/com/jandrevl/pressuresaver/MainActivity.java
+++ app/src/main/java/com/jandrevl/pressuresaver/MainActivity.java
@@ -3,6 +3,7 @@ package com.jandrevl.pressuresaver;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SQLiteDatabase myDataBase = openOrCreateDatabase("pressureSaverDataBase", MODE_PRIVATE, null);
+        myDataBase.execSQL("CREATE TABLE IF NOT EXISTS measurements(systolic INT, diastolic INT, pulse INT, date VARCHAR);");
     }
 
     public void registerMeasurement(View view) {
--- app/src/main/java/com/jandrevl/pressuresaver/RegisterActivity.java
+++ app/src/main/java/com/jandrevl/pressuresaver/RegisterActivity.java
@@ -2,13 +2,31 @@ package com.jandrevl.pressuresaver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText systolic;
+    EditText diastolic;
+    EditText pulse;
+    EditText date;
+    EditText time;
+    Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+    }
+
+    public void saveMeasurement(View view) {
+
+
+        finish();
     }
 }
