9bb36dc9390894e2af1bb96ff0f1764d18b7f6ab
Joao Lopes <jandrevl@gmail.com>
2022-10-20 13:59:20 +0100

    Continuing developing the RegisterActivity class

--- app/src/main/java/com/jandrevl/pressuresaver/RegisterActivity.java
+++ app/src/main/java/com/jandrevl/pressuresaver/RegisterActivity.java
@@ -4,23 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText systolic;
-    EditText diastolic;
-    EditText pulse;
-    EditText date;
-    EditText time;
+    EditText systolicEditText;
+    EditText diastolicEditText;
+    EditText pulseEditText;
+    EditText dateEditText;
+    EditText timeEditText;
     Button save;
+    LocalDate date = LocalDate.now();
+    LocalTime time = LocalTime.now();
+    String dateString;
+    String timeString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        dateEditText = findViewById(R.id.dateEditText);
+        timeEditText = findViewById(R.id.timeEditText);
+        dateString = date.toString();
+        timeString = time.format(DateTimeFormatter.ofPattern("HH:mm"));
+
+        dateEditText.setText(dateString);
+        timeEditText.setText(timeString);
 
     }
 
